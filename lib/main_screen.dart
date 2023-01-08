66d78d708394ba6337e4191c8df9cbee2fdc87a3
oh-naoki <oh.naoki0819@gmail.com>
2023-01-08 13:05:42 +0900

    Add empty main screen

---
+++ lib/main_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Zenn Reader'),
+      ),
+      body: const Center(
+        child: Text('body'),
+      ),
+    );
+  }
+}
