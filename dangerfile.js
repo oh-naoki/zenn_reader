9874a3afc3da6a5e5a638e9f832bdab1d3c70cc3
oh-naoki <oh.naoki0819@gmail.com>
2023-02-12 17:07:42 +0900

    fix

--- dangerfile.js
+++ dangerfile.js
@@ -1,7 +1,4 @@
 import {message, danger} from "danger"
 import { flutterAnalyze } from 'dangerjs-flutter-analyze-plugin'
 
-const modifiedMD = danger.git.modified_files.join("- ")
-message("Changed Files in this PR: \n - " + modifiedMD)
-
 flutterAnalyze()
