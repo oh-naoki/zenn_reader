c77b5da1b66df199c2f68fcd6b18fd4dbb7f4ce5
oh-naoki <oh.naoki0819@gmail.com>
2023-02-12 16:07:01 +0900

    Use original analyze plugin for dange

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,61 +1,7 @@
 import {message, danger} from "danger"
+import { flutterAnalyze } from 'dangerjs-flutter-analyze-plugin'
 
 const modifiedMD = danger.git.modified_files.join("- ")
 message("Changed Files in this PR: \n - " + modifiedMD)
 
-const report_txt = fs.readFileSync('./flutter_analyze_report.txt', 'utf-8').split('\n').map(line_txt => line_txt.trim());
-
-const reports = report_txt.filter(line => checkSeverity(line)).map(report => {
-    const splitReport = report.split(" • ");
-    return new AnalyzeResult(
-        Severity.INFO,
-        splitReport[1] as string,
-        splitReport[2].split(":")[0] as string,
-        splitReport[2].split(":")[1] as unknown as number,
-    )
-});
-
-reports.forEach(value => {
-    switch (value.severity) {
-        case Severity.INFO:
-            message(value.description, value.fileName, value.line);
-            break;
-        case Severity.ERROR:
-        case Severity.WARNING:
-            warn(value.description, value.fileName, value.line);
-    }
-});
-
-enum Severity {
-    INFO = "info",
-    WARNING = "warning",
-    IGNORE = "ignor",
-    ERROR = "error",
-}
-
-class AnalyzeResult {
-    severity: Severity
-    description: String
-    fileName: String
-    line: number
-    constructor(
-        severity: Severity,
-        description: String,
-        fileName: String,
-        line: number,
-    ) {
-        this.severity = severity
-        this.description = description
-        this.fileName = fileName
-        this.line = line
-    }
-}
-
-function checkSeverity(value: string): boolean {
-    for (let key in Severity) {
-        if (value.startsWith(Severity[key as keyof typeof Severity])) {
-            return true;
-        }
-    }
-    return false;
-}
+flutterAnalyze()
